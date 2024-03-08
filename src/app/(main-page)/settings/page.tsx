8e2996de4131ff351f5123c3e58c6817d531acca
ReformaMark <mark.reforma0117@gmail.com>
2024-03-08 18:52:20 +0800

    Merge branch 'master' of https://github.com/DrrylPns/agrishare

--- src/app/(main-page)/settings/page.tsx
+++ src/app/(main-page)/settings/page.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import { AccountSettings } from "./_components/AccountSettings"
+import { TraderAddress } from "./_components/TraderAddress"
+import { PasswordSettings } from "./_components/PasswordSettings"
+import { getAuthSession } from "@/lib/auth"
+import prisma from "@/lib/db"
+import { User } from "@prisma/client"
 
-function page() {
-  return (
-    <div>Settings page</div>
-  )
+const SettingsPage = async () => {
+    const session = await getAuthSession()
+
+    const user = await prisma.user.findFirst({
+        where: {
+            id: session?.user.id
+        }
+    })
+
+    return (
+        <main className="w-full">
+            {/* Account Settings */}
+            <AccountSettings user={user as User} />
+
+            {/* Trader Address */}
+            <TraderAddress user={user as User} />
+
+            {/* Change Password */}
+            <PasswordSettings />
+        </main>
+    )
 }
 
-export default page
+export default SettingsPage
