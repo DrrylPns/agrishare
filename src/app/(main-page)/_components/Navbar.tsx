ba70e096c112869c52c5321f2d03c965c13532d7
DrrylPns <56997112+DrrylPns@users.noreply.github.com>
2024-03-12 20:13:59 +0800

    optimized responsiveness

--- src/app/(main-page)/_components/Navbar.tsx
+++ src/app/(main-page)/_components/Navbar.tsx
@@ -1,14 +1,28 @@
 "use client";
 import { Logo } from '@/components/logo';
-import { buttonVariants } from '@/components/ui/button';
+import { Button, buttonVariants } from '@/components/ui/button';
 import { UserAccountAvatar } from '@/components/user-account-avatar';
 import { cn } from '@/lib/utils';
-import { useSession } from 'next-auth/react';
+import { signOut, useSession } from 'next-auth/react';
 import Link from 'next/link';
 import React, { useState } from 'react'
-import { BiMenu } from 'react-icons/bi';
+import { BiMenu, BiSolidDonateHeart } from 'react-icons/bi';
 import { SearchInput } from './SearchInput';
-import { CiBellOn, CiHeart } from 'react-icons/ci';
+import { CiBellOn, CiHeart, CiLogout, CiSettings } from 'react-icons/ci';
+import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+} from "@/components/ui/sheet"
+import { usePathname } from 'next/navigation';
+import { MdDashboard, MdHistory } from 'react-icons/md';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { TbClover2 } from 'react-icons/tb';
+import { FaRegFileAlt } from 'react-icons/fa';
+
 
 function Navbar() {
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
@@ -17,100 +31,151 @@ function Navbar() {
     const toggleMobileMenu = () => {
         setIsMobileMenuOpen(!isMobileMenuOpen);
     };
-  return (
-    <nav
-    className={`bg-white fixed flex justify-around items-center z-50 top-0 left-0 w-full h-[70px] px-4 sm:px-20 py-4 shadow transition-color duration-500 ${visible ? "" : "opacity-0"
-        }`}
->
-    <div className="w-1/3 h-14 ">
-        <Logo />
-    </div>
-    <div>
-        <SearchInput/>
-    </div>
-    {status === "unauthenticated" ? (
-        <div className="flex flex-row gap-3 max-sm:hidden">
-            <Link
-                href="/sign-in"
-                className={cn(buttonVariants({ variant: "primary" }), "p-3 px-8 rounded-full")}
-            >
-                Sign In
-            </Link>
+    const pathname = usePathname()
+    return (
+        <nav
+            className={`bg-white fixed flex justify-around items-center z-50 top-0 left-0 w-full h-[70px] px-4 sm:px-20 py-4 shadow transition-color duration-500 ${visible ? "" : "opacity-0"
+                }`}
+        >
+            <div className='hidden md:block'>
+                <Logo />
+            </div>
 
-            <Link
-                href="/sign-up"
-                className={cn(buttonVariants({ variant: "primary" }), "p-3 px-8 rounded-full")}
-            >
-                Sign Up
-            </Link>
-        </div>
-    ) : status === "authenticated" ? (
-        <div className="max-sm:hidden flex items-center text-3xl gap-3 ">
-            <Link 
-                href={'/agrineeds'} 
-                className='hover:scale-105'
-            >
-                <CiHeart />
-               
-            </Link>
-            <CiBellOn />
-            <UserAccountAvatar />
-        </div>
-    ) : status === "loading" ? (
-        <div>Loading...</div>
-    ) : (<></>)}
+            <div>
+                <SearchInput />
+            </div>
+
+            {status === "unauthenticated" ? (
+                <div className="flex flex-row gap-3 max-sm:hidden">
+                    <Link
+                        href="/sign-in"
+                        className={cn(buttonVariants({ variant: "primary" }), "p-3 px-8 rounded-full")}
+                    >
+                        Sign In
+                    </Link>
+
+                    <Link
+                        href="/sign-up"
+                        className={cn(buttonVariants({ variant: "primary" }), "p-3 px-8 rounded-full")}
+                    >
+                        Sign Up
+                    </Link>
+                </div>
+            ) : status === "authenticated" ? (
+                <div className="max-sm:hidden flex items-center text-3xl gap-3 ">
+                    <Link
+                        href={'/agrineeds'}
+                        className='hover:scale-105'
+                    >
+                        <CiHeart />
+
+                    </Link>
+                    <CiBellOn />
+                    <UserAccountAvatar />
+                </div>
+            ) : status === "loading" ? (
+                <div className='hidden md:block'>Loading...</div>
+            ) : (<></>)}
 
 
 
-    {/* Mobile dropdown menu */}
-    <button
-        className="sm:hidden text-black text-[2rem]"
-        onClick={toggleMobileMenu}
-    >
-        <BiMenu />
-    </button>
+            {/* Mobile dropdown menu */}
+            <Sheet>
+                <SheetTrigger className='md:hidden block'>
+                    <BiMenu className='text-[32px]' />
+                </SheetTrigger>
+                <SheetContent className=''>
+                    <SheetHeader>
+                        <SheetTitle className='w-full flex justify-center items-center'>
+                            <Logo />
+                        </SheetTitle>
+                        <SheetDescription>
+                            <div className='text-gray-500 transition-all duration-700 ease-in-out mt-3'>
+                                <Link
+                                    href={'/agrifeed'}
+                                    className={`link ${pathname === '/agrifeed' ? "border-l-2 border-primary-green bg-[#e6e6e671]" : ""} flex gap-3 w-full items-center justify-center hover:bg-[#e6e6e671] py-3`}
+                                >
+                                    <span><MdDashboard /></span>
+                                    <h1>Agrifeed</h1>
 
-    {isMobileMenuOpen && (
-        <div className="sm:hidden fixed left-0 w-full top-[70px] h-[40%] flex items-center justify-center shadow">
-            <ul className="text-black text-[16px] text-center">
-                <li
-                    className=""
-                    onClick={() => {
-                        toggleMobileMenu();
-                    }}
-                >
-                    Home
-                </li>
-                <li
-                    className="my-5"
-                    onClick={() => {
-                        toggleMobileMenu();
-                    }}
-                >
-                    About Us
-                </li>
-                <li
-                    className="my-5"
-                    onClick={() => {
-                        toggleMobileMenu();
-                    }}
-                >
-                    News
-                </li>
-                <li
-                    className="my-5"
-                    onClick={() => {
-                        toggleMobileMenu();
-                    }}
-                >
-                    Contact Us
-                </li>
-            </ul>
-        </div>
-    )}
+                                </Link>
+                                <Link
+                                    href={'/catergories'}
+                                    className={`link ${pathname === '/catergories' ? "border-l-2 border-primary-green bg-[#e6e6e671]" : ""} flex gap-3 w-full items-center justify-center hover:bg-[#e6e6e671] py-3`}
+                                >
+                                    <span><GiHamburgerMenu /></span>
+                                    <h1>Catergories</h1>
+                                </Link>
+                                <Link
+                                    href={'/donation'}
+                                    className={`link ${pathname === '/donation' ? "border-l-2 border-primary-green bg-[#e6e6e671]" : ""} flex gap-3 w-full items-center justify-center hover:bg-[#e6e6e671] py-3`}
+                                >
+                                    <span><BiSolidDonateHeart /></span>
+                                    <h1>Donation</h1>
 
-</nav>
-  )
+                                </Link>
+                                <Link
+                                    href={'/points'}
+                                    className={`link ${pathname === '/points' ? "border-l-2 border-primary-green bg-[#e6e6e671]" : ""} flex gap-3 w-full items-center justify-center hover:bg-[#e6e6e671] py-3`}
+                                >
+                                    <span><TbClover2 /></span>
+                                    <h1>Points</h1>
+
+                                </Link>
+                                <Link
+                                    href={'/agrineeds'}
+                                    className={`link ${pathname === '/agrineeds' ? "border-l-2 border-primary-green bg-[#e6e6e671]" : ""} flex gap-3 w-full items-center justify-center hover:bg-[#e6e6e671] py-3`}
+                                >
+                                    <span><CiHeart /></span>
+                                    <h1>Agrineeds</h1>
+                                </Link>
+                                <Link
+                                    href={'/news'}
+                                    className={`link ${pathname === '/news' ? "border-l-2 border-primary-green bg-[#e6e6e671]" : ""} flex gap-3 w-full items-center justify-center hover:bg-[#e6e6e671] py-3`}
+                                >
+                                    <span><FaRegFileAlt /></span>
+                                    <h1>News</h1>
+                                </Link>
+                                <Link
+                                    href={'/history'}
+                                    className={`link ${pathname === '/history' ? "border-l-2 border-primary-green bg-[#e6e6e671]" : ""} flex gap-3 w-full items-center justify-center hover:bg-[#e6e6e671] py-3`}
+                                >
+                                    <span><MdHistory /></span>
+                                    <h1>History</h1>
+                                </Link>
+                                <Link
+                                    href={'/settings'}
+                                    className={`link ${pathname === '/settings' ? "border-l-2 border-primary-green bg-[#e6e6e671]" : ""} flex gap-3 w-full items-center justify-center hover:bg-[#e6e6e671] py-3`}
+                                >
+                                    <span><CiSettings /></span>
+                                    <h1>Settings</h1>
+                                </Link>
+                                {status === 'loading' && (
+                                    <></>
+                                )}
+                                {status === 'authenticated' && (
+                                    <Button
+                                        onClick={() => { signOut() }}
+                                        variant={'outline'}
+                                        className='flex gap-3 w-full items-center justify-center hover:bg-[#e6e6e671] py-3'
+                                    >
+                                        <span><CiLogout /></span>
+                                        <h1>Logout</h1>
+                                    </Button>
+                                )}
+
+                            </div>
+                        </SheetDescription>
+                    </SheetHeader>
+                </SheetContent>
+            </Sheet>
+
+
+
+
+
+        </nav>
+    )
 }
 
 export default Navbar
--- src/app/(main-page)/agrifeed/_components/ProductCard.tsx
+++ src/app/(main-page)/agrifeed/_components/ProductCard.tsx
@@ -7,7 +7,7 @@ import {
     CardFooter,
     CardHeader,
     CardTitle,
-  } from "@/components/ui/card"
+} from "@/components/ui/card"
 import Image from 'next/image';
 import { Button } from '@/components/ui/button';
 import { LiaExchangeAltSolid } from "react-icons/lia";
@@ -23,12 +23,12 @@ function ProductCard({
     category,
     tag,
     availableStocks,
-}:{
+}: {
     id: number;
-    user:{
+    user: {
         id: string;
         name: string;
-    } ;
+    };
     productImage: string;
     productName: string;
     description: string;
@@ -36,46 +36,46 @@ function ProductCard({
     tag: string[];
     availableStocks: number;
 }) {
-  return (
-    <Card className='transition-all duration-700 ease-in-out border-gray-400 border p-10 rounded-2xl drop-shadow-md hover:drop-shadow-md hover:shadow-xl font-poppins'>
-        <CardTitle className='text-4xl font-semibold'>{user.name}</CardTitle>
+    return (
+        <Card className='transition-all duration-700 ease-in-out border-gray-400 border p-10 rounded-2xl drop-shadow-md hover:drop-shadow-md hover:shadow-xl font-poppins'>
+            <CardTitle className='text-4xl font-semibold'>{user.name}</CardTitle>
 
-        <div className='flex justify-around gap-5 mt-5'>
-            <Image 
-            src={productImage} 
-            alt={productName}
-            width={300}
-            height={300}
-            className='w-2/5 h-72 object-center border border-gray-300'
-            />
-            <div className='w-1/2'>
-                <div className='flex items-center gap-5 mb-3'>
-                    <h1 className='text-3xl font-medium'>{productName}</h1>
-                    <span className='px-3 py-1 font-bold bg-green-200 text-[0.5rem] text-green-400'>{availableStocks < 1 ? 'Not Available' : "In Stock"}</span>
+            <div className='flex justify-around gap-5 mt-5'>
+                <Image
+                    src={productImage}
+                    alt={productName}
+                    width={300}
+                    height={300}
+                    className='w-2/5 h-72 object-center border border-gray-300'
+                />
+                <div className='w-1/2'>
+                    <div className='flex items-center gap-5 mb-3'>
+                        <h1 className='text-3xl font-medium'>{productName}</h1>
+                        <span className='px-3 py-1 font-bold bg-green-200 text-[0.5rem] text-green-400'>{availableStocks < 1 ? 'Not Available' : "In Stock"}</span>
+                    </div>
+                    <CardDescription className='min-h-24 line-clamp-5 text-ellipsis'>{description}</CardDescription>
+                    <div className='flex gap-3 justify-between items-center border-y-2 border-gray-300 py-5'>
+                        <Button variant={'default'} className='rounded-full w-2/5'>
+                            Trade
+                            <span className='ml-3'><LiaExchangeAltSolid /></span>
+                        </Button>
+                        <Button variant={'default'} className='rounded-full  w-2/5'>
+                            Message
+                            <span className='ml-3'><BiMessageDetail /></span>
+                        </Button>
+                        <Button variant={'ghost'} className='rounded-full px-3 text-lg font-medium bg-gray-300'>
+                            <span className=''><CiHeart /></span>
+                        </Button>
+                    </div>
+                    <h1 className='text-sm text-gray-700 my-3'>Categoy: <span className='text-gray-500'>{category}</span></h1>
+                    <h1 className='text-sm text-gray-700'>Tag/s:
+                        {tag.length > 0 && tag.map((tag, index) => (
+                            <span className='text-gray-500' key={index}>{tag} </span>
+                        ))}</h1>
                 </div>
-                <CardDescription className='min-h-24 line-clamp-5 text-ellipsis'>{description}</CardDescription>
-                <div className='flex gap-3 justify-between items-center border-y-2 border-gray-300 py-5'>
-                    <Button variant={'default'} className='rounded-full w-2/5'>
-                        Trade 
-                        <span className='ml-3'><LiaExchangeAltSolid /></span>
-                    </Button>
-                    <Button variant={'default'} className='rounded-full  w-2/5'>
-                        Message 
-                        <span className='ml-3'><BiMessageDetail /></span>
-                    </Button>
-                    <Button variant={'ghost'} className='rounded-full px-3 text-lg font-medium bg-gray-300'> 
-                        <span className=''><CiHeart /></span>
-                    </Button>
-                </div>
-                <h1 className='text-sm text-gray-700 my-3'>Categoy: <span className='text-gray-500'>{category}</span></h1>
-                <h1 className='text-sm text-gray-700'>Tag/s: 
-                    {tag.length > 0 && tag.map((tag)=>(
-                        <span className='text-gray-500'>{tag} </span>
-                    ))}</h1>
             </div>
-        </div>
-    </Card>
-  )
+        </Card>
+    )
 }
 
 export default ProductCard
