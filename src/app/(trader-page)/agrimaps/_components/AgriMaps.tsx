006f9450f5cd8e228ae9c349ce2b105adc189ce6
DrrylPns <56997112+DrrylPns@users.noreply.github.com>
2024-04-22 16:39:27 +0800

    agrimaps part 2

--- src/app/(trader-page)/agrimaps/_components/AgriMaps.tsx
+++ src/app/(trader-page)/agrimaps/_components/AgriMaps.tsx
@@ -1,16 +1,24 @@
 "use client"
 import React, { useCallback, useState } from 'react'
-import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+import { GoogleMap, InfoWindow, Marker, useJsApiLoader } from '@react-google-maps/api';
 
 const containerStyle = {
-    width: '400px',
+    width: '100%',
     height: '400px'
 };
 
 const center = {
-    lat: -3.745,
-    lng: -38.523
+    lat: 14.715310577164843,
+    lng: 121.03522441433974
 };
+
+const coordinates = [
+    { lat: 14.715310577164843, lng: 121.03522441433974, name: "Sharon Farm" },
+    { lat: 14.731483158808185, lng: 121.0620389676612, name: "Villa Viena Urban Farm" },
+    { lat: 14.709614652698521, lng: 121.04723620103312, name: "Urban Farm of Calderon" },
+    { lat: 14.735558467177205, lng: 121.03526207751709, name: "Solo Parent Urban Farming" },
+    // Add more coordinates here...
+];
 
 export const AgriMaps = () => {
     const { isLoaded } = useJsApiLoader({
@@ -19,9 +27,9 @@ export const AgriMaps = () => {
     })
 
     const [map, setMap] = useState(null)
+    const [selectedMarker, setSelectedMarker] = useState(null);
 
     const onLoad = useCallback(function callback(map: any) {
-        // This is just an example of getting and using the map instance!!! don't just blindly copy!
         const bounds = new window.google.maps.LatLngBounds(center);
         map.fitBounds(bounds);
 
@@ -32,16 +40,39 @@ export const AgriMaps = () => {
         setMap(null)
     }, [])
 
+    const handleMarkerClick = (marker: any) => {
+        setSelectedMarker(marker);
+    };
+
     return isLoaded ? (
         <GoogleMap
             mapContainerStyle={containerStyle}
             center={center}
-            zoom={10}
+            zoom={12}
             onLoad={onLoad}
             onUnmount={onUnmount}
         >
-            { /* Child components, such as markers, info windows, etc. */}
-            <></>
+            {coordinates.map((coordinate, index) => (
+                <Marker
+                    key={index}
+                    position={coordinate}
+                    onClick={() => handleMarkerClick(coordinate)}
+                >
+                    {selectedMarker === coordinate && (
+                        <InfoWindow
+                            position={coordinate}
+                            onCloseClick={() => setSelectedMarker(null)}
+                        >
+                            <div>
+                                <h3>{
+                                    // @ts-ignore
+                                    coordinate.name}</h3>
+                                {/* You can add additional information here if needed */}
+                            </div>
+                        </InfoWindow>
+                    )}
+                </Marker>
+            ))}
         </GoogleMap>
     ) : <></>
 }
