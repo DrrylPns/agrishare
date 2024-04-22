e02e49067a76ca98ccee0de311f8c112eb9152f1
DrrylPns <56997112+DrrylPns@users.noreply.github.com>
2024-04-23 04:25:40 +0800

    agrimaps addons

--- src/app/(trader-page)/agrimaps/_components/AgriMaps.tsx
+++ src/app/(trader-page)/agrimaps/_components/AgriMaps.tsx
@@ -17,7 +17,13 @@ const coordinates = [
     { lat: 14.731483158808185, lng: 121.0620389676612, name: "Villa Viena Urban Farm" },
     { lat: 14.709614652698521, lng: 121.04723620103312, name: "Urban Farm of Calderon" },
     { lat: 14.735558467177205, lng: 121.03526207751709, name: "Solo Parent Urban Farming" },
-    // Add more coordinates here...
+    { lat: 14.700575329668167, lng: 121.03357004390666, name: "Center for Urban Agriculture and Innovation (QCU)" },
+    { lat: 14.69261616209439, lng: 121.0752362291887, name: "Gulayan at Bulaklakan Urban Farm" },
+    { lat: 14.689517343159308, lng: 121.05601712073572, name: "SunnyVille Urban Farm" },
+    { lat: 14.658201721231105, lng: 121.0552264935998, name: "Green Thumb Arbo Urban Farm (GrowQC)" },
+    { lat: 14.648421426484312, lng: 121.05098021719552, name: "Joy of Urban Farming" },
+    { lat: 14.652738473934496, lng: 121.05672069016371, name: "San Vicente Urban Farming" },
+
 ];
 
 export const AgriMaps = () => {
