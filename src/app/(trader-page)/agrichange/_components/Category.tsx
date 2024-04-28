461aeefaf47b5e89054451267371b11d815ecee9
DrrylPns <56997112+DrrylPns@users.noreply.github.com>
2024-04-28 19:56:55 +0800

    fix: agrishare visibility bug

--- src/app/(trader-page)/agrichange/_components/Category.tsx
+++ src/app/(trader-page)/agrichange/_components/Category.tsx
@@ -20,67 +20,67 @@ import { BeatLoader } from "react-spinners";
 
 
 const Categories = [
-    'FRESH_FRUIT',
+  'FRESH_FRUIT',
   'VEGETABLES',
   'TOOLS',
   'EQUIPMENTS',
   'SEEDS',
   'SOILS',
   'FERTILIZER',
-   
-  ]
+
+]
 
 export default function Category({
 
-}:{
+}: {
 
-}) {
-    const [selectedCategory, setSelectedCategory] = useState<string>(Categories[0])
-    const [isLoading, setIsLoading] = useState<boolean>(true)
-    const [post, setPost] = useState<Agrichange[]>()
-    const [currentPage, setCurrentPage] = useState(1);
-    const [itemsPerPage, setItemsPerPage] = useState(9);
-    const [selectedSort, setSelectedSort] = useState<string>('Latest');
+  }) {
+  const [selectedCategory, setSelectedCategory] = useState<string>(Categories[0])
+  const [isLoading, setIsLoading] = useState<boolean>(true)
+  const [post, setPost] = useState<Agrichange[]>()
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage, setItemsPerPage] = useState(9);
+  const [selectedSort, setSelectedSort] = useState<string>('Latest');
 
-    const lastItemIndex = currentPage * itemsPerPage;
-    const firstItemIndex = lastItemIndex - itemsPerPage;
-    const currentItems = post?.slice(firstItemIndex, lastItemIndex)
-    
-    useEffect(() => {
-      getPostsByCategory();
-      setCurrentPage(1)
-  }, [selectedCategory, selectedSort]); 
+  const lastItemIndex = currentPage * itemsPerPage;
+  const firstItemIndex = lastItemIndex - itemsPerPage;
+  const currentItems = post?.slice(firstItemIndex, lastItemIndex)
 
-   const getPostsByCategory = async () => {
+  useEffect(() => {
+    getPostsByCategory();
+    setCurrentPage(1)
+  }, [selectedCategory, selectedSort]);
+
+  const getPostsByCategory = async () => {
     setIsLoading(true);
     try {
-        const res = await axios.post('/api/getAgrichangePost', {
-            category: selectedCategory,
-            sort: selectedSort 
-        });
-        const filteredPosts = res.data.filter((item : Agrichange)  => item.quantity >= item.quantityPerTrade);
-        setPost(filteredPosts);
+      const res = await axios.post('/api/getAgrichangePost', {
+        category: selectedCategory,
+        sort: selectedSort
+      });
+      // const filteredPosts = res.data.filter((item: Agrichange) => item.quantity >= item.quantityPerTrade);
+      setPost(res.data);
     } catch (error) {
-        console.log(error);
+      console.log(error);
     } finally {
-        setIsLoading(false);
+      setIsLoading(false);
     }
-}
+  }
 
   return (
     <>
-    <div className='flex justify-between'>
-      <div className='relative flex flex-col md:flex-row md:items-center justify-around'>
-        <h1 className="text-xs sm:text-sm mr-2 sm:mr-5">Sort by:</h1>
-        <Listbox value={selectedSort} onChange={setSelectedSort}>
-          <Listbox.Button className={'relative w-28 sm:w-40 py-1 md:py-3 text-xs md:text-lg rounded-md flex items-center justify-around bg-white shadow-md ui-open::ring-2 ring-black'}>{selectedSort} <IoIosArrowDown /></Listbox.Button>
-          <Transition
-            as={Fragment}
-            leave="transition ease-in duration-100"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            <Listbox.Options className="absolute top-10 sm:top-14 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
+      <div className='flex justify-between'>
+        <div className='relative flex flex-col md:flex-row md:items-center justify-around'>
+          <h1 className="text-xs sm:text-sm mr-2 sm:mr-5">Sort by:</h1>
+          <Listbox value={selectedSort} onChange={setSelectedSort}>
+            <Listbox.Button className={'relative w-28 sm:w-40 py-1 md:py-3 text-xs md:text-lg rounded-md flex items-center justify-around bg-white shadow-md ui-open::ring-2 ring-black'}>{selectedSort} <IoIosArrowDown /></Listbox.Button>
+            <Transition
+              as={Fragment}
+              leave="transition ease-in duration-100"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Listbox.Options className="absolute top-10 sm:top-14 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
                 <Listbox.Option
                   value={'Latest'}
                   className='relative cursor-default select-none py-2 px-2 md:pl-10 md:pr-4 text-xs md:text-lg hover:bg-slate-100 text-gray-900'
@@ -93,81 +93,81 @@ export default function Category({
                 >
                   Transaction
                 </Listbox.Option>
-            </Listbox.Options>
-          </Transition>
-        </Listbox>
-      </div>
-      <div className='md:hidden relative flex flex-col md:flex-row md:items-center justify-around'>
-        <h1 className="text-xs sm:text-sm mr-2 sm:mr-5">All Categories:</h1>
-        <Listbox value={selectedCategory} onChange={setSelectedCategory}>
-          <Listbox.Button className={'relative w-28 sm:w-40 py-1 md:py-3 bg-green-400 text-white tracking-wider text-xs md:text-lg rounded-md flex items-center justify-around shadow-md ui-open::ring-2 ring-black'}>{formattedCategory(selectedCategory)} <IoIosArrowDown /></Listbox.Button>
-          <Transition
-            as={Fragment}
-            leave="transition ease-in duration-100"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            <Listbox.Options className="absolute top-10 sm:top-14 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
-                {Categories.map((category)=>(
-                    <Listbox.Option
-                      value={category}
-                      className='relative cursor-default select-none py-2 px-2 md:pl-10 md:pr-4 text-xs hover:bg-slate-100 text-gray-900'
-                    >
+              </Listbox.Options>
+            </Transition>
+          </Listbox>
+        </div>
+        <div className='md:hidden relative flex flex-col md:flex-row md:items-center justify-around'>
+          <h1 className="text-xs sm:text-sm mr-2 sm:mr-5">All Categories:</h1>
+          <Listbox value={selectedCategory} onChange={setSelectedCategory}>
+            <Listbox.Button className={'relative w-28 sm:w-40 py-1 md:py-3 bg-green-400 text-white tracking-wider text-xs md:text-lg rounded-md flex items-center justify-around shadow-md ui-open::ring-2 ring-black'}>{formattedCategory(selectedCategory)} <IoIosArrowDown /></Listbox.Button>
+            <Transition
+              as={Fragment}
+              leave="transition ease-in duration-100"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Listbox.Options className="absolute top-10 sm:top-14 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
+                {Categories.map((category) => (
+                  <Listbox.Option
+                    value={category}
+                    className='relative cursor-default select-none py-2 px-2 md:pl-10 md:pr-4 text-xs hover:bg-slate-100 text-gray-900'
+                  >
                     {formattedCategory(category)}
-                    </Listbox.Option>
+                  </Listbox.Option>
                 ))}
-                
-            </Listbox.Options>
-          </Transition>
-        </Listbox>
+
+              </Listbox.Options>
+            </Transition>
+          </Listbox>
+        </div>
       </div>
-    </div>
-    <div className='grid grid-cols-12 w-full gap-x-2 '>
-      <h1 className="text-right col-span-12 my-3 md:col-span-9 text-[0.6rem] sm:text-sm">{post?.length} Results found</h1>
+      <div className='grid grid-cols-12 w-full gap-x-2 '>
+        <h1 className="text-right col-span-12 my-3 md:col-span-9 text-[0.6rem] sm:text-sm">{post?.length} Results found</h1>
         <div className='grid grid-cols-2 md:grid-cols-3 w-full  gap-3 col-span-12 sm:col-span-9 '>
           {isLoading ? (
             <div className='w-full flex items-center justify-center'>
               <BeatLoader />
             </div>
           ) : (
-          <>
-          {currentItems && currentItems.length > 0 ? currentItems.map((item)=>(
-            <div key={item.id}>
-              <PostCard post={item} />
-            </div>
-            )):(
             <>
-              No Items found
+              {currentItems && currentItems.length > 0 ? currentItems.map((item) => (
+                <div key={item.id}>
+                  <PostCard post={item} />
+                </div>
+              )) : (
+                <>
+                  No Items found
+                </>
+              )}
             </>
-          )}
-          </>
           )}
         </div>
         <div className='col-span-3 hidden md:block'>
-            <RadioGroup value={selectedCategory} onChange={setSelectedCategory} className="shadow-md drop-shadow-md p-5">
-                      <RadioGroup.Label className="text-lg font-semibold">All Categories:</RadioGroup.Label>
-                      {Categories.map((category) => (
-                        <RadioGroup.Option key={category} value={category} className={`flex ml-5 mt-3 items-center gap-4 text-xl font-poppins`}>
-                           {({ checked }) => (
-                            <>
-                            <span className={`${checked && 'text-green-400'} text-lg`}>
-                              {checked ? <IoIosRadioButtonOn  /> : <IoIosRadioButtonOff /> }
-                            
-                            </span>
-                            {formattedCategory(category)}
-                            </>
-                            )}
-                        </RadioGroup.Option>
-                      ))}
-            </RadioGroup>
+          <RadioGroup value={selectedCategory} onChange={setSelectedCategory} className="shadow-md drop-shadow-md p-5">
+            <RadioGroup.Label className="text-lg font-semibold">All Categories:</RadioGroup.Label>
+            {Categories.map((category) => (
+              <RadioGroup.Option key={category} value={category} className={`flex ml-5 mt-3 items-center gap-4 text-xl font-poppins`}>
+                {({ checked }) => (
+                  <>
+                    <span className={`${checked && 'text-green-400'} text-lg`}>
+                      {checked ? <IoIosRadioButtonOn /> : <IoIosRadioButtonOff />}
+
+                    </span>
+                    {formattedCategory(category)}
+                  </>
+                )}
+              </RadioGroup.Option>
+            ))}
+          </RadioGroup>
         </div>
-    </div>
-    <PaginationSection
-      totalItems={post?.length}
-      itemsPerPage={itemsPerPage}
-      currentPage={currentPage}
-      setCurrentPage={setCurrentPage}
-    />
+      </div>
+      <PaginationSection
+        totalItems={post?.length}
+        itemsPerPage={itemsPerPage}
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+      />
     </>
   )
 };
