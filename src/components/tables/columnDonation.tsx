5d99e33952dd438973c660b3d7109be511658e3a
ReformaMark <mark.reforma0117@gmail.com>
2024-05-01 23:36:54 +0800

    chore: change condirate neautral from Poor,Neutral,Good to Poor,Good,Best

--- src/components/tables/columnDonation.tsx
+++ src/components/tables/columnDonation.tsx
@@ -296,8 +296,8 @@ export const columnDonation: ColumnDef<DonationWithDonators>[] = [
                                                                 >
                                                                     <CheckIcon className="hidden ui-checked:block" height={20} width={20} />
                                                                     {rate === 0.5 && "Poor"}
-                                                                    {rate === 1 && "Neutral"}
-                                                                    {rate === 1.5 && "Good"}
+                                                                    {rate === 1 && "Good"}
+                                                                    {rate === 1.5 && "Best"}
                                                                 </RadioGroup.Option>
                                                             ))}
                                                         </div>
