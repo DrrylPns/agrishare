6c5403565f0a3dfb8e732a7a679c869fc219a219
ReformaMark <mark.reforma0117@gmail.com>
2024-05-02 04:58:54 +0800

    chore: edit role action

--- src/app/(admin)/users/_components/columns.tsx
+++ src/app/(admin)/users/_components/columns.tsx
@@ -5,6 +5,13 @@ import { ColumnDef } from "@tanstack/react-table"
 import { DataTableColumnHeader } from "./data-table-column-header"
 import { format } from "date-fns"
 import { categories } from "@/lib/utils"
+import { useState, useTransition } from "react"
+import { DropdownMenu, DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu"
+import { Button } from "@/components/ui/button"
+import { MoreHorizontalIcon } from "lucide-react"
+import { DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator } from "@/components/ui/dropdown-menu"
+import { Dialog, DialogContent, DialogDescription, DialogHeader } from "@/components/ui/dialog"
+import { DialogTitle } from "@radix-ui/react-dialog"
 
 export const columns: ColumnDef<User>[] = [
     {
@@ -33,7 +40,7 @@ export const columns: ColumnDef<User>[] = [
         },
         cell: ({ row }) => {
             const name = row.original.name
-            const lastName = row.original.lastName
+            const lastName = row.original.lastName 
 
             const words = name?.split(' ');
 
@@ -108,4 +115,56 @@ export const columns: ColumnDef<User>[] = [
             </div>
         },
     },
+    {
+        id: "actions",
+        header: "Actions",
+        cell: ({ row }) => {
+            const [isReviewOpen, setIsReviewOpen] = useState<boolean>()
+            const [isPending, startTransition] = useTransition()
+            const [isConfirmOpen, setIsConfirmOpen] = useState<boolean>(false)
+            const [isProofOpen, setIsProofOpen] = useState<boolean>()
+            const [isDownloadOpen, setIsDownloadOpen] = useState<boolean>()
+            const [isRejectOpen, setIsRejectOpen] = useState<boolean>(false)
+            const [imageUrl, setImageUrl] = useState<string>("")
+
+          
+
+            return (
+                <>
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <Button variant="ghost" className="h-8 w-8 p-0">
+                                <span className="sr-only">Open menu</span>
+                                <MoreHorizontalIcon className="h-4 w-4" />
+                            </Button>
+                        </DropdownMenuTrigger>
+                            <DropdownMenuContent align="end">
+                                <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                                <DropdownMenuSeparator />
+                                <DropdownMenuItem
+                                    className="cursor-pointer"
+                                    onClick={() => setIsReviewOpen(true)}
+                                >
+                                    Edit role
+                                </DropdownMenuItem>
+                            </DropdownMenuContent>
+                    </DropdownMenu>
+                    
+                    <Dialog open={isReviewOpen} onOpenChange={setIsReviewOpen}>
+                        <DialogContent className="lg:max-w-2xl" >
+                            <DialogHeader>
+                                <DialogTitle>
+                                 
+                                </DialogTitle>
+                                <DialogDescription>
+                                    
+                                </DialogDescription>
+                            </DialogHeader>
+                        </DialogContent>
+                    </Dialog>
+                   
+                </>
+            )
+        },
+    }
 ]
