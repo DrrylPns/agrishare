424bc181ce78604a02e6df942c365a07b05c7f63
DrrylPns <56997112+DrrylPns@users.noreply.github.com>
2024-05-04 03:13:54 +0800

    feat: urban farm list on admin

--- src/app/(admin)/add-agrimaps/page.tsx
+++ src/app/(admin)/add-agrimaps/page.tsx
@@ -1,16 +1,21 @@
 import AdminTitle from '@/components/AdminTitle'
-import { Card } from '@/components/ui/card'
+import { Card, CardHeader, CardTitle } from '@/components/ui/card'
 import AgrimapsForm from './_components/AgrimapsForm'
-import prisma from '@/lib/db';
+import prisma from '@/lib/db';;
+import { UrbanFarmList } from './_components/UrbanFarmList';
 
-const AddAgrimapsPage = () => {
-
+const AddAgrimapsPage = async () => {
+    const coordinates = await prisma.coordinates.findMany();
     return (
         <div className='h-full'>
             <AdminTitle entry='7' title='Agrimaps' />
-
             <Card className='mx-auto max-w-full h-full drop-shadow-lg p-6 mb-11'>
                 <AgrimapsForm />
+            </Card>
+
+            <AdminTitle entry='8' title='List of Urban Farms' />
+            <Card className='mx-auto max-w-full h-full drop-shadow-lg p-6 mb-11'>
+                <UrbanFarmList coordinates={coordinates} />
             </Card>
         </div>
     )
